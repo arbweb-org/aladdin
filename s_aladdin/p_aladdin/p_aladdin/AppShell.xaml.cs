37287c55751239016dfe661f734b27971a050718
yassin-soliman <yassin_soliman@outlook.com>
2020-08-27 22:53:32 +0200

    Initial commit

---
+++ s_aladdin/p_aladdin/p_aladdin/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace p_aladdin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ s_aladdin/p_aladdin/p_aladdin/Views/_c_home.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace p_aladdin
+{
+    public partial class _c_home : ContentPage
+    {
+        public _c_home()
+        {
+            InitializeComponent();
+        }
+    }
+}
